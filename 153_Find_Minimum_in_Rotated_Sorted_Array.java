cbc5f729ea461d75d278b92ef6d642788910409c
Faiyazthulla Shaik <faiyazshaik03@gmail.com>
2023-08-05 19:21:45 -0700

    Updating using Binary Search

--- 153_Find_Minimum_in_Rotated_Sorted_Array.java
+++ 153_Find_Minimum_in_Rotated_Sorted_Array.java
@@ -1,11 +1,24 @@
 class Solution {
     public int findMin(int[] nums) {
-        int min = nums[0];
-        for(int i=0; i<nums.length; i++){
-            min = Math.min(min, nums[i]);
+        if(nums.length == 0) return 0;
+
+        int left = 0;
+        int right = nums.length-1;
+
+        while(left<right){
+            int midPoint = left+(right-left)/2;
+
+            if(midPoint>0 && nums[midPoint] < nums[midPoint-1]){
+                return nums[midPoint];
+            }else if(nums[left] <= nums[midPoint] && nums[midPoint] > nums[right]){
+                left = midPoint+1;
+            }else{
+                right = midPoint-1;
+            }
         }
-        return min;
+        return nums[left];
     }
 }
 
-//Time complexity: O(n)
+//Time complexity: O(log N)
+//Space complexity: O(1)
