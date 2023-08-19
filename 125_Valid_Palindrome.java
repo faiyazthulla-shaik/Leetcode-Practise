205b86371d02959b72070bfa1030fef76871de62
Faiyazthulla Shaik <faiyazshaik03@gmail.com>
2023-08-19 14:02:49 -0700

    Updating the directories.

--- 125_Valid_Palindrome.java
+++
@@ -1,8 +0,0 @@
-class Solution {
-    public boolean isPalindrome(String s) {
-        s = s.replaceAll(" ", "").toLowerCase().replaceAll("[^a-zA-Z0-9]", "");
-        StringBuilder strBu = new StringBuilder();
-        if(s.equals(strBu.append(s).reverse().toString())) return true;
-        return false;
-    }
-}
