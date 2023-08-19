490b23ffd644eeef282882c75f10639173bca0a9
Faiyazthulla Shaik <faiyazshaik03@gmail.com>
2023-08-19 13:49:17 -0700

    Adding 977_Squares_of_a_Sorted_Array.java file

---
+++ Normal_Practise_Problems/977_Squares_of_a_Sorted_Array.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+        int[] result = new int[nums.length];
+        for(int i=nums.length-1; i>=0; i--){
+            int square;
+            if(Math.abs(nums[left]) < Math.abs(nums[right])){
+                square = nums[right];
+                right--;
+            }else{
+                square = nums[left];
+                left++;
+            }
+            result[i] = square * square;
+        }
+        return result;
+    }
+}
