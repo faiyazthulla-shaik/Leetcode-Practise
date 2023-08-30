f747b7589fdc6d625c595746199768668c58c8fc
Faiyazthulla Shaik <faiyazshaik03@gmail.com>
2023-08-30 14:11:00 -0700

    Adding 53_Maximum_Subarray.java file

---
+++ Blind_75/53_Maximum_Subarray.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int sum = 0;
+        int result = Integer.MIN_VALUE;
+        for(int i=0; i<nums.length; i++){
+            if(sum <=0 && sum <=nums[i]){
+                sum = 0;
+            }
+            sum = sum + nums[i];
+            result = Math.max(result, sum);
+        }
+        return result;
+    }
+}
