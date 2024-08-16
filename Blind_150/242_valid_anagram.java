43bb0b0fce129c808ca4dfbb842569c2b81f1c52
Faiyazthulla Shaik <faiyazshaik03@gmail.com>
2024-08-16 16:13:32 -0700

    Updating the 242_valid_anagram.java file

--- Blind_150/242_valid_anagram.java
+++ Blind_150/242_valid_anagram.java
@@ -1,26 +1,22 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if(s.length() != t.length()) return false;
-        HashMap<Character, Integer> hashMapS = new HashMap<>();
-        HashMap<Character, Integer> hashMapT = new HashMap<>();
-
-        for(int i=0; i<s.length(); i++){
-            if(!hashMapS.containsKey(s.charAt(i))){
-                hashMapS.put(s.charAt(i), 1);
-            }else if(hashMapS.containsKey(s.charAt(i))){
-                hashMapS.put(s.charAt(i), hashMapS.get(s.charAt(i))+1);
-            }
-            
-            if(!hashMapT.containsKey(t.charAt(i))){
-                hashMapT.put(t.charAt(i), 1);
-            }else if(hashMapT.containsKey(t.charAt(i))){
-                hashMapT.put(t.charAt(i), hashMapT.get(t.charAt(i))+1);
+        if (s.length() != t.length()) {
+            return false;
+        }
+        int[] table = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            table[s.charAt(i) - 'a']++;
+            table[t.charAt(i) - 'a']--;
+        }
+    
+        for (int i = 0; i < table.length; i++) {
+            if (table[i] < 0) {
+                return false;
             }
         }
-        if(hashMapT.equals(hashMapS)) return true;
-        return false;
+        return true;
     }
 }
 
 //Time complexity: O(n)
-//Space complexity: O(n)
+//Space complexity: O(1)
