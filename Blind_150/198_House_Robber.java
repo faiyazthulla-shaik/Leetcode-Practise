b168c3683e3b604d871e9cd890a0e694a12e4328
Faiyazthulla Shaik <faiyazshaik03@gmail.com>
2024-08-16 13:23:11 -0700

    Updating the 198_House_Robber.java file

--- Blind_150/198_House_Robber.java
+++ Blind_150/198_House_Robber.java
@@ -13,3 +13,18 @@ class Solution {
 
 //Time complexity: O(n)
 //Space complexity: O(n)
+
+class Solution {
+    public int rob(int[] nums) {
+        int rob1 = 0, rob2 = 0, newRob = 0;
+        for(int i=0; i<nums.length; i++){
+            newRob = Math.max(rob1+nums[i], rob2);
+            rob1 = rob2;
+            rob2 = newRob;
+        }
+        return newRob;
+    }
+}
+
+//Time complexity: O(n)
+//Space complexity: O(1)
