c9b58d28d1bb29c476c1f0586cbc8d3ec025139f
Faiyazthulla Shaik <faiyazshaik03@gmail.com>
2024-09-03 20:04:38 -0700

    Updating 153_Find_Minimum_in_Rotated_Sorted_Array.java file

--- Blind_150/153_Find_Minimum_in_Rotated_Sorted_Array.java
+++ Blind_150/153_Find_Minimum_in_Rotated_Sorted_Array.java
@@ -1,20 +1,20 @@
 class Solution {
     public int findMin(int[] nums) {
-        if(nums.length == 0 || nums == null) return 0;
-        if(nums[0]<nums[nums.length-1] || nums.length == 1) return nums[0];
+        if(nums.length == 1 || nums[0]<nums[nums.length-1]) return nums[0];
+
         int left = 0;
-        int right = nums.length-1;
+        int right = nums.length - 1;
         while(left<=right){
             int mid = left+(right-left)/2;
-            if(mid>0 && nums[mid]<nums[mid-1]) return nums[mid];
-            if(nums[mid]>nums[mid+1]) return nums[mid+1];
+            if(nums[mid] > nums[mid+1]) return nums[mid+1];
+            if(nums[mid] < nums[mid-1]) return nums[mid];
             if(nums[left]<nums[mid]){
                 left = mid+1;
             }else{
                 right = mid-1;
             }
         }
-        return -1;
+        return Integer.MAX_VALUE;
     }
 }
 
